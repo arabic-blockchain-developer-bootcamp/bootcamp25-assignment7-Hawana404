50892a6919b60c02d97c3d73eef6c67f25bb8944
Hawana404 <youssef.hawana@ejust.edu.eg>
2025-04-20 11:28:42 +0200

    so done

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -1,11 +1,11 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Assignment7 is ERC20 {
     // Constructor to initialize the token
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    constructor() ERC20("Hawana", "HWA") {
         
         // Initialize the token name and symbol
         
